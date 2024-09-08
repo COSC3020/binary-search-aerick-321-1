6d8bdabb7d0bb183f533ad966ec5bf55463fa472
aerick-321 <158105735+aerick-321@users.noreply.github.com>
2024-09-08 15:47:36 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,17 @@
 function binarySearch(list, element) {
+    let start = 0;
+    let end = list.length - 1; 
+    while (start <= end) {
+        var mid = (start + end) / 2;
+        if (mid == element) {
+            return mid;
+        }
+        else if (mid < element) {
+            start = mid + 1;
+        }
+        else {
+            end = mid - 1;
+        }
+    }
     return -1;
 }
